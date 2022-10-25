c8afff05046ad22e78c727566f550030f45126c9
José Miguel Chávez Galdamez <jose.chavez.g@usach.cl>
2022-10-24 21:23:30 -0300

    New/POST page HTML

--- react/react-app-example/src/pages/employees/New.js
+++ react/react-app-example/src/pages/employees/New.js
@@ -1,6 +1,30 @@
-import { Container,Row,Col, Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Container,Row,Col, Form, Button, InputGroup, Alert } from "react-bootstrap";
 
 const NewEmployee = () => {
+    const [showAlert, setShowAlert] = useState(false);
+    const [invalidForm, setInvalidForm] = useState(false);
+
+    const handleNewEmployeeForm = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        if (form.checkValidity() === true) {
+            let form_data = new FormData(form);
+            // let data = Object.fromEntries(form_data.entries());
+            // POST -> Create Employee
+            setShowAlert(true);
+            window.setTimeout(()=>{
+                setShowAlert(false);
+                setInvalidForm(false);
+                form.reset();
+            },3000);
+        } else{
+            event.stopPropagation();
+            setInvalidForm(true);
+        }
+    };
+
+
     return (
         <Container style={{display: 'flex', marginTop: '70px'}}>
             <Row>
@@ -8,26 +32,83 @@ const NewEmployee = () => {
                     <h1>Nuevo Empleado</h1>
                 </Col>
                 <Col>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
+                    <Form noValidate validated={invalidForm} onSubmit={handleNewEmployeeForm}>
+                        <Row>
+                            <Form.Group className="mb-3" controlId="identification" >
+                                <Form.Label>Rut</Form.Label>
+                                <Form.Control
+                                    type="input"
+                                    placeholder="12345678-2"
+                                    name="identification"
+                                    required
+                                />
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Col cols="6">
+                                <Form.Group className="mb-3" controlId="first_name">
+                                    <Form.Label>Nombre</Form.Label>
+                                    <Form.Control
+                                        type="input"
+                                        placeholder="Juan"
+                                        name="first_name"
+                                        required
+                                    />
+                                </Form.Group>
+                            </Col>
+                            <Col cols="6">
+                                <Form.Group className="mb-3" controlId="last_name">
+                                    <Form.Label>Apellido</Form.Label>
+                                    <Form.Control
+                                        type="input"
+                                        placeholder="Perez"
+                                        name="last_name"
+                                        required
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Form.Group className="mb-3" controlId="entrance_date">
+                                <Form.Label>Fecha Ingreso</Form.Label>
+                                <Form.Control
+                                    type="input"
+                                    placeholder="22-01-2022"
+                                    name="entrance_date"
+                                    required
+                                />
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group className="mb-3" controlId="base_salary">
+                                <Form.Label>Salario Base</Form.Label>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text id="money-addon">$</InputGroup.Text>
+                                    <Form.Control
+                                        placeholder="0"
+                                        aria-label="Salario"
+                                        aria-describedby="money-addon"
+                                        type="number"
+                                        name="base_salary"
+                                        required
+                                    />
+                                    </InputGroup>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Col cols="6">
+                                <Button variant="primary" type="submit">Crear Empleado</Button>
+                            </Col>
+                            <Col cols="6" >
+                                <Button style={{float: 'right'}} variant="secondary" href="/employees">
+                                    Volver a lista de empleados
+                                </Button>
+                            </Col>
+                        </Row>
                     </Form>
+                    <Alert variant="success" className="mt-4" show={showAlert} >
+                        Usuario Creado correctamente
+                    </Alert>
                 </Col>
             </Row>
         </Container>
